1624bc060c69ecd21104ad8ff2d0a464d0d28721
Matthew John Cheetham <mjcheetham@outlook.com>
2017-12-23 15:40:17 +0000

    Fix typo in comment

--- src/isr.h
+++ src/isr.h
@@ -6,5 +6,5 @@ typedef struct registers
     uint32 ds; // data segment selector
     uint32 edi, esi, ebp, esp, ebx, edx, ecx, eax; // pushed by pusha
     uint32 int_no, err_code; // interrupt number and error code (if applicable)
-    uint32 eip, cs, eflags, useresp, ss; // pushed by the processor on automatically (iret)
+    uint32 eip, cs, eflags, useresp, ss; // pushed by the processor automatically (on iret)
 } registers_t;
