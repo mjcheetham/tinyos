a71640f6761b56b480b942bae8d20909727960fa
Matthew John Cheetham <mjcheetham@outlook.com>
2018-09-11 16:28:24 +0100

    Set up CI with Azure Pipelines

--- etc/build-linker.sh
+++ etc/build-linker.sh
@@ -13,7 +13,6 @@ LD="$OUT/bin/i386-elf-ld"
 
 # GNU binutils compilation options
 TARGET=i386-elf
-PREFIX=`cd $OUT ; pwd`
 
 main()
 {
@@ -36,16 +35,19 @@ main()
 		tar -xf $TARFILE --directory $PKGROOT
 	fi
 
-	mkdir -p $GEN $OUT
+	mkdir -p $OUT
+	prefix=`cd $OUT && pwd`
+
+	mkdir -p $GEN
 	pushd $GEN > /dev/null
 
 	echo "Configuring compilation..."
 	echo -e "\ttarget:\t$TARGET"
-	echo -e "\tprefix:\t$PREFIX"
-	../binutils-2.26/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX &> configure.log
+	echo -e "\tprefix:\t$prefix"
+	../binutils-2.26/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$prefix > configure.log
 
 	echo "Compiling linker..."
-	make all install &> make.log
+	make all install > make.log
 
 	popd > /dev/null
 }
